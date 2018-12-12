7674ff9b09c913de19e34ff7164eee553932f01a
zmf <1984654893@qq.com>
2018-12-12 22:03:35 +0800

    [REF] 解决返回commit时间问题

---
+++ src/main/java/io/choerodon/gitlab/api/dto/CommitDTO.java
@@ -0,0 +1,166 @@
+package io.choerodon.gitlab.api.dto;
+
+import org.gitlab4j.api.models.Author;
+import org.gitlab4j.api.models.CommitStats;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author zmf
+ */
+public class CommitDTO {
+    private Author author;
+    private Date authoredDate;
+    private String authorEmail;
+    private String authorName;
+    private String committedDate;
+    private String committerEmail;
+    private String committerName;
+    private String createdAt;
+    private String id;
+    private String message;
+    private List<String> parentIds;
+    private String shortId;
+    private CommitStats stats;
+    private String status;
+    private Date timestamp;
+    private String title;
+    private String url;
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Date getAuthoredDate() {
+        return authoredDate;
+    }
+
+    public void setAuthoredDate(Date authoredDate) {
+        this.authoredDate = authoredDate;
+    }
+
+    public String getAuthorEmail() {
+        return authorEmail;
+    }
+
+    public void setAuthorEmail(String authorEmail) {
+        this.authorEmail = authorEmail;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getCommittedDate() {
+        return committedDate;
+    }
+
+    public void setCommittedDate(String committedDate) {
+        this.committedDate = committedDate;
+    }
+
+    public String getCommitterEmail() {
+        return committerEmail;
+    }
+
+    public void setCommitterEmail(String committerEmail) {
+        this.committerEmail = committerEmail;
+    }
+
+    public String getCommitterName() {
+        return committerName;
+    }
+
+    public void setCommitterName(String committerName) {
+        this.committerName = committerName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getParentIds() {
+        return parentIds;
+    }
+
+    public void setParentIds(List<String> parentIds) {
+        this.parentIds = parentIds;
+    }
+
+    public String getShortId() {
+        return shortId;
+    }
+
+    public void setShortId(String shortId) {
+        this.shortId = shortId;
+    }
+
+    public CommitStats getStats() {
+        return stats;
+    }
+
+    public void setStats(CommitStats stats) {
+        this.stats = stats;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
