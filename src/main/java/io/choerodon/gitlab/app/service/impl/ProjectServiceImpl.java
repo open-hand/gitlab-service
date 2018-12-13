3679c8e2acc43d733dae9c1e24ed543d0399e39a
10980 <zhiyang.zhu@hand-china.com>
2018-12-13 13:37:08 +0800

    [FIX] fix some bug

--- src/main/java/io/choerodon/gitlab/app/service/impl/ProjectServiceImpl.java
+++ src/main/java/io/choerodon/gitlab/app/service/impl/ProjectServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.exception.FeignException;
 import io.choerodon.gitlab.api.dto.MemberDto;
 import io.choerodon.gitlab.app.service.ProjectService;
@@ -91,7 +92,7 @@ public class ProjectServiceImpl implements ProjectService {
         try {
             return gitlab4jclient.getGitLabApi(userId).getProjectApi().getProject(groupCode, projectCode);
         } catch (GitLabApiException e) {
-            throw new FeignException(e.getMessage(), e);
+            throw new CommonException(e.getMessage(), e);
         }
     }
 
