d92fcd84f2bb18ecf7aaa8efed2b97d79d482dd7
lizhaozhong <lzz2234@163.com>
2019-08-21 16:53:12 +0800

    [ADD] 获取gitlab中admin用户的token

--- src/main/java/io/choerodon/gitlab/api/controller/v1/ConfigController.java
+++ src/main/java/io/choerodon/gitlab/api/controller/v1/ConfigController.java
@@ -1,8 +1,9 @@
 package io.choerodon.gitlab.api.controller.v1;
 
 import io.choerodon.core.exception.CommonException;
+import io.choerodon.gitlab.app.service.ConfigService;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,18 +20,19 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/v1/confings")
 public class ConfigController {
-    @Value("${gitlab.privateToken}")
-    private String privateToken;
+
+    @Autowired
+    private ConfigService configService;
 
     /**
      * 获取配置文件gitlab组件admin的token
      *
-     * @return
+     * @return token
      */
     @ApiOperation(value = "获取配置文件gitlab组件admin的token")
-    @GetMapping("/getAdminToken")
+    @GetMapping("/get_admin_token")
     public ResponseEntity<String> getAdminToken() {
-        return Optional.ofNullable(privateToken)
+        return Optional.ofNullable(configService.getAdminToken())
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElseThrow(() -> new CommonException("error.admin.token.get"));
     }
